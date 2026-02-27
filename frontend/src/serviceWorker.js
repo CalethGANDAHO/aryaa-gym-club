3cf98ee225324008aa35b5306a7e3cf420f0d85d
CalethGANDAHO <gandahocaleth7@gmail.com>
2026-02-27 17:05:04 +0100

    Create serviceWorker.js file for PWA caching strategy

---
+++ frontend/src/serviceWorker.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+        // Add any other assets you want to cache
+      ]);
+    })
+  );
+});
+
+// Fetch from Cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== 'v1') {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
